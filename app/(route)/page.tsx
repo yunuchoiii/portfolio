08dbd4fd1d33c2835e090e33e244d404baaeca30
Seowon Choi <chltjdnjs529@gmail.com>
2024-05-06 21:57:25 +0900

    Feat: background blur effect 조정

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -2,6 +2,7 @@
 
 import { useWindowSize } from "@uidotdev/usehooks";
 import { useState } from "react";
+import { useRecoilValue } from "recoil";
 import About from "../_components/About/About";
 import Contact from "../_components/Contact/Contact";
 import Home from "../_components/Home/Home";
@@ -9,8 +10,10 @@ import FullPage from "../_components/Layout/Page/FullPage";
 import Skills from "../_components/Skills/Skills";
 import Works from "../_components/Works/Works";
 import { SIDEBAR_WIDTH } from "../_constants";
+import { activeSectionAtom } from "../_store/activeSection";
 
 export default function Main() {
+  const activeSection = useRecoilValue(activeSectionAtom)
 
   const windowSize = useWindowSize()
   const isMobile = windowSize.width! <= 768
@@ -29,8 +32,8 @@ export default function Main() {
 
   return (
     <div 
-      className="relative scroll-container-y h-screen overflow-x-hidden"
-      style={{paddingLeft: !isMobile ? SIDEBAR_WIDTH : 0 }}
+      className={`relative scroll-container-y h-screen overflow-x-hidden bg-white bg-opacity-[0.01] transition-all duration-700 ${activeSection == "Home" || activeSection == "Contact" ? "backdrop-blur-0" :  "2xl:backdrop-blur-[75px] lg:backdrop-blur-[55px] sm:backdrop-blur-[45px] backdrop-blur-[35px] 2xl:dark:backdrop-blur-[100px] lg:dark:backdrop-blur-[75px] sm:dark:backdrop-blur-[57px] dark:backdrop-blur-[40px]"}`}
+      style={{marginLeft: !isMobile ? SIDEBAR_WIDTH : 0 }}
     >
       <FullPage id="Home">
         <Home/>
