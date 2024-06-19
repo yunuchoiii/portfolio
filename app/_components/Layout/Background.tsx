40b0b94b48bcc5c00e9c0a182f8b4f734949a95a
Seowon Choi <seowon@medifoodplatform.com>
2024-06-19 13:57:12 +0900

    Revert "feat: background Image 태그 적용"
    This reverts commit 600c65a7d0b97e2dcbf30e630fd572ac75cfab54.

--- app/_components/Layout/Background.tsx
+++ app/_components/Layout/Background.tsx
@@ -3,7 +3,6 @@
 import { FIRST_LOADING_TIME_MS } from "@/app/_constants";
 import { isRenderingStateAtom } from "@/app/_store/isRendering";
 import { useIntersectionObserver } from "@uidotdev/usehooks";
-import Image from "next/image";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import BarLoader from "../Loader/BarLoader";
@@ -24,22 +23,8 @@ const Background = () => {
   }, [])
 
   return <div ref={ref} className={`fixed w-screen h-screen ${isRendering ? "z-50" : "-z-10"}`}>
-    {/* <img src="/images/main/home-bg-light.png" className={`block dark:hidden w-full h-full object-top object-cover transition-all duration-[2s] ${entry?.intersectionRatio ? "scale-100" : "scale-[2]"}`}/>
-    <img src="/images/main/home-bg-dark.png" className={`hidden dark:block w-full h-full object-top object-cover transition-all duration-[2s] ${entry?.intersectionRatio ? "scale-100" : "scale-[2]"}`}/> */}
-    <Image
-      src="/images/main/home-bg-light.png" 
-      alt="bg-light"
-      className={`block dark:hidden w-full h-full object-top object-cover transition-all duration-[2s] ${entry?.intersectionRatio ? "scale-100" : "scale-[2]"}`}
-      width={3000}
-      height={2000}
-    />
-    <Image 
-      src="/images/main/home-bg-dark.png" 
-      alt="bg-dark"
-      className={`hidden dark:block w-full h-full object-top object-cover transition-all duration-[2s] ${entry?.intersectionRatio ? "scale-100" : "scale-[2]"}`}
-      width={3000}
-      height={2000}
-    />
+    <img src="/images/main/home-bg-light.png" className={`block dark:hidden w-full h-full object-top object-cover transition-all duration-[2s] ${entry?.intersectionRatio ? "scale-100" : "scale-[2]"}`}/>
+    <img src="/images/main/home-bg-dark.png" className={`hidden dark:block w-full h-full object-top object-cover transition-all duration-[2s] ${entry?.intersectionRatio ? "scale-100" : "scale-[2]"}`}/>
     {isRendering && <div className="absolute top-0 left-0 z-[60] w-full h-full flex items-center justify-center">
       <BarLoader/>
     </div>}
